3b4489cdc22013df660ab182c71475a284c2e6fe
fellowbeard <virgovibe@gmail.com>
2022-03-07 17:30:09 -1000

    adds new methods to contacts app

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,13 @@
+class ContactsController < ApplicationController
+
+  def get_one_contact
+    contacts = Contact.first
+    render json: contacts.as_json
+  end
+
+  def get_all_contacts
+    contacts = Contact.all 
+    render json: contacts.as_json
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+
+    get "/one_contact", controller: "contacts",
+    action: "get_one_contact"
+
+    get "/all_contacts", controller: "contacts", 
+    action: "get_all_contacts"
+
+end
