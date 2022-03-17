df330105328e4c14fdca1fb1801fd06c588f5a6f
fellowbeard <virgovibe@gmail.com>
2022-03-17 13:03:20 -1000

    new stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+contact1 = Contact.create(first_name: "Alice", last_name: "Cooper", email: "shoutatthedevil@test.com", phone_number: "6666666")
+
+contact2 = Contact.create(first_name: "Ozzy", last_name: "Osborn", email: "ieatbatheads@test.com", phone_number: "6666667")
+
+contact3 = Contact.create(first_name: "Wayne", last_name: "Newton", email: "allofme@test.com", phone_number: "1234567")
+
+puts "done!!"
+
