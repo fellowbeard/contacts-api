f25f1fade6adcace2e31dac7261d7799042c9643
fellowbeard <virgovibe@gmail.com>
2022-03-21 16:29:50 -1000

    updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 Rails.application.routes.draw do
-
+    ### contacts
     get "/contacts" => "contacts#index"
-
     post "/contacts" => "contact#create"
-
     get "/contacts/:id" => "contacts#show"
-    
     patch "/contacts" => "contacts#create"
-
     post "/contacts" => "contacts#create"
 
+    ### Users
     post "/users" => "users#create"
-
+    
+    ### Sessions
     post "/sessions" => "sessions#create"
 
 end
